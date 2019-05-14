a60b1b53bcacc0016d8f8c3d76a7e73f66f05b15
Jonathan Peclat <peclatj@bluewin.ch>
2019-05-15 00:09:55 +0200

    create user app

---
+++ user_app.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from model import Conv1DNet2
+from spectrogram import Spectrogram
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        
+        self.spectrogram = Spectrogram()
+        self.model = Conv1DNet2()
+        
+        self.musicFilePath = StringVar()
+        self.defaultDir = "/home/peclatj/data/Document/Université/Master MCS/Advanced Topics in Machine Learning/ATML19/genres_wav/classical"
+
+
+        self.create_widgets()        
+
+
+    def create_widgets(self):
+        self.chooseFileBtn = tk.Button(self)
+        self.chooseFileBtn["text"] = "ChooseFile"
+        self.chooseFileBtn["command"] = self.chooseFile
+        self.chooseFileBtn.pack(side="top")
+
+        self.filePathPan = PanedWindow(self, orient=HORIZONTAL, width=1000)
+        self.filePathPan.pack(side="top")
+        
+        self.filePath1Lbl = tk.Label(self.filePathPan, text="File path:")
+        self.filePathPan.add(self.filePath1Lbl)
+        
+        self.filePath2Lbl = tk.Label(self.filePathPan, textvariable=self.musicFilePath)
+        self.filePathPan.add(self.filePath2Lbl)
+        
+        self.classifyBtn = tk.Button(self)
+        self.classifyBtn["text"] = "Classify"
+        self.classifyBtn["command"] = self.classify
+        self.classifyBtn.pack(side="top")
+
+        self.quit = tk.Button(self, text="Quit", fg="red", command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def chooseFile(self):
+        filetypes = (("wave files","*.wav"),("all files","*.*"))
+        path = filedialog.askopenfilename(initialdir = self.defaultDir, title="Select a music", filetypes=filetypes)
+        self.musicFilePath.set(path)
+
+    def classify(self):
+        imgs = self.spectrogram.sample(self.musicFilePath.get())
+        
+        self.model.load("best_model")
+        
+        for img in imgs:
+            print(self.model.predict_image(img))
+
+
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
