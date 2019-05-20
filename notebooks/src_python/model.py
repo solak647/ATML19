b08da8ea55255f8a7546708ab114cb98f8d318a3
Thomas Schaller <thomasschaller647@gmail.com>
2019-05-20 10:20:23 +0200

    generate experiments

---
+++ notebooks/src_python/model.py
@@ -0,0 +1,113 @@
+import torch.nn as nn
+import torch
+from torchvision.transforms import Resize, ToTensor, Normalize, Compose, Grayscale, ColorJitter
+from torch.autograd import Variable
+from torchvision import models
+
+class Model():
+    
+    def __init__(self):
+        self.transforms = Compose([
+                        #Grayscale(num_output_channels=3),
+                        Resize((216,216)),
+                        ToTensor(),           # Converts to Tensor, scales to [0, 1] float (from [0, 255] int)
+                        Normalize((0.5,), (0.5,)), # scales to [-1.0, 1.0]
+                      ])
+        
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        
+    def load(self, filepath):
+        self.model_ft = models.resnet18()
+        num_ftrs = self.model_ft.fc.in_features
+        self.model_ft.fc = nn.Linear(num_ftrs, 10)
+        self.model_ft.load_state_dict(torch.load(filepath, map_location=self.device))
+        for parameter in self.model_ft.parameters():
+            parameter.requires_grad = False
+        self.model_ft.eval()
+
+    def predict_image(self, image):
+        image_tensor = self.transforms(image).float()
+        image_tensor = image_tensor.unsqueeze_(0)
+        input = Variable(image_tensor)
+        input = input.to(self.device)
+        output = nn.Softmax(dim=0)(self.model_ft(input)[0])
+        return output.data.cpu().numpy()
+
+# class Conv1DNet2(nn.Module):
+    
+#     def __init__(self):
+#         super(Conv1DNet2, self).__init__()
+        
+#         self.transforms = Compose([
+#                         Grayscale(num_output_channels=1),
+#                         ToTensor(),           # Converts to Tensor, scales to [0, 1] float (from [0, 255] int)
+#                         Normalize((0.5,), (0.5,)), # scales to [-1.0, 1.0]
+#                       ])
+        
+#         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        
+#         self.conv = nn.Sequential(
+#           # input: 1x216x216
+#           nn.Conv2d(1, 128, (5,1)),
+#           # output: 128x212x216
+#           nn.BatchNorm2d(128,momentum=0.9),
+#           nn.LeakyReLU(0.2),
+#           nn.MaxPool2d(2),
+#           nn.Dropout(0.5),
+#           # output: 128x106x108
+#           nn.Conv2d(128, 64, (5,1)),
+#           # output: 64x102x108
+#           nn.BatchNorm2d(64,momentum=0.9),
+#           nn.LeakyReLU(0.2),
+#           nn.MaxPool2d(2),
+#           nn.Dropout(0.5),
+#           # output: 64x51x54
+#           nn.Conv2d(64, 64, (4,1)),
+#           # output: 64x48x54
+#           nn.BatchNorm2d(64,momentum=0.9),
+#           nn.LeakyReLU(0.2),
+#           nn.MaxPool2d(2),
+#           nn.Dropout(0.5),
+#           # output: 64x24x27
+#           nn.Conv2d(64, 64, (5,1)),
+#           # output: 64x20x27
+#           nn.BatchNorm2d(64,momentum=0.9),
+#           nn.LeakyReLU(0.2),
+#           nn.MaxPool2d((2,1), (2,1)),
+#           nn.Dropout(0.5)
+#           # output: 64x10x27
+#         )
+#         self.fc = nn.Sequential(
+#           nn.Linear(64*10*27,364),
+#           nn.LeakyReLU(0.2),
+#           nn.Dropout(0.5),
+#           nn.Linear(364,192),
+#           nn.LeakyReLU(0.2),
+#           nn.Dropout(0.5),
+#           nn.Linear(192,10)
+#         )
+    
+#     def forward(self, input):
+#         output = self.conv(input)
+#         output = output.view(output.size(0), 64*10*27)
+#         output = self.fc(output)
+#         return output
+        
+#     def load(self, filepath):
+#         self.load_state_dict(torch.load(filepath, map_location=self.device))
+#         for parameter in self.parameters():
+#             parameter.requires_grad = False
+#         self.eval()
+
+#     def predict_image(self, image):
+#         image.save('out.png')
+#         image_tensor = self.transforms(image).float()
+#         image_tensor = image_tensor.unsqueeze_(0)
+#         input = Variable(image_tensor)
+#         input = input.to(self.device)
+#         output = self(input)
+#         index = output.data.cpu().numpy().argmax()
+#         return index
+            
---
+++ notebooks/src_python/spectrogram.py
@@ -0,0 +1,69 @@
+import scipy.io.wavfile as wavfile
+import matplotlib.pyplot as plt
+import numpy as np
+
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import urllib, base64
+import pylab
+import sys
+import os
+from PIL import Image
+from io import BytesIO
+
+class Spectrogram:
+    
+    def __init__(self):
+    
+        # number of frequency bins (vertical resolution of the spectrogram)
+        self.n_mels = 128
+        
+        self.n_fft=2048
+        self.hop_length=512
+        self.power=2.0
+        
+        # window size
+        self.window_size = 100000
+        self.window_step =  70000
+    
+        pass
+    
+    def sample(self, filepath):
+        sample_rate, samples = wavfile.read(filepath)
+        
+        # make the sample mono
+        samples = samples[:,0]
+        
+        n_rolls = int((len(samples) - self.window_size) / self.window_step)
+        
+        # normalize samples
+        samples = samples / np.max(np.abs(samples))
+
+        # split the sample    
+        splits = []
+        imgs = []
+        
+        for roll in range(n_rolls):
+            splits.append(samples[:self.window_size])
+            samples = np.roll(samples, -self.window_step)
+
+        for i, split in enumerate(splits):
+            S = librosa.feature.melspectrogram(y=split, sr=sample_rate,
+                n_mels=self.n_mels, n_fft=self.n_fft,
+                hop_length=self.hop_length, power=self.power)
+
+            pylab.figure(figsize=(3,3))
+            pylab.axis('off') 
+            pylab.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[]) # Remove the white edge
+            librosa.display.specshow(librosa.power_to_db(S, ref=np.max), y_axis='mel', fmax=8000, x_axis='time', cmap="gray")
+            buf = BytesIO()
+            pylab.savefig(buf, format='png', bbox_inches=None, pad_inches=0)
+            buf.seek(0);
+            
+            img = Image.open(buf)
+            imgs.append(img.convert('RGB'))
+            pylab.clf()
+            pylab.close()
+            #pylab.close() # Crashes the GUI
+        return imgs
